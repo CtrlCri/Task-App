4c83b8a7f2c0b7522c0159428b1eaf7c0a16d026
CtrlCri <aramayoreyes@gmail.com>
2023-09-19 12:09:07 -0300

    created migrations

--- app/core_api/settings.py
+++ app/core_api/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': os.getenv('DATABASE_NAME'), 
         'USER': os.getenv('DATABASE_USER'), 
         'PASSWORD': os.getenv('DATABASE_PASS'),
-        'HOST': "db", 
+        'HOST': 'localhost',  # Cambia "db" por "localhost" o "127.0.0.1"
         "PORT": 5432, 
     }
 }
